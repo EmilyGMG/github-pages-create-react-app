7de66ad2402efa90236cba93ee148a3b580c506d
EmilyGMG <emily.maria2010@hotmail.com>
2021-03-03 16:56:27 -0300

    teste-2

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import App from './Pages/App';
 ReactDOM.render(
 
   <Router>
-  <div>
-    <Route exact path='/' component={App} />
-    <Route path='/edit/:id' component={Edit} />
-    <Route path='/create' component={Create} />
-    <Route path='/show/:id' component={Show} />
-  </div>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/edit/:id' component={Edit} />
+      <Route path='/create' component={Create} />
+      <Route path='/show/:id' component={Show} />
+    </div>
 </Router>,
   
   
