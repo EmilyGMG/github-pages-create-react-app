9d199e3d8e17e9c3a74a03c523c21d75164221fc
EmilyGMG <emily.maria2010@hotmail.com>
2021-03-03 16:15:47 -0300

    Updates

---
+++ precache-manifest.8297e0469b48b2ff2de888b1c2d0fb09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cb7c6f7d2e14ada2b2c8e2272896afa",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "afc7e0919646dbeb5f2b",
+    "url": "/github-pages-create-react-app/static/css/main.d96a0f7e.chunk.css"
+  },
+  {
+    "revision": "a709f183d4d8772285cd",
+    "url": "/github-pages-create-react-app/static/js/2.7950fe00.chunk.js"
+  },
+  {
+    "revision": "7220ec57ae4adfde121066bca8a2c846",
+    "url": "/github-pages-create-react-app/static/js/2.7950fe00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "afc7e0919646dbeb5f2b",
+    "url": "/github-pages-create-react-app/static/js/main.4524fd2f.chunk.js"
+  },
+  {
+    "revision": "a693770ba3fa0de86c97",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.ad67307e.js"
+  }
+]);
